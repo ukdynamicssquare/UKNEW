e7ce705ddca0b5c7f607571991e3b98af66eb37c
Sushil Behera <sbehera@mpgbis.com>
2024-01-10 15:21:12 +0530

    update finance operation

--- pages/products/microsoft-dynamics-365-finance-operations.js
+++ pages/products/microsoft-dynamics-365-finance-operations.js
@@ -1,9 +1,135 @@
 import Link from "next/link";
+import { useState } from 'react';
 import Head from "next/head";
 import Image from "next/image";
 import Blog from "../../components/Blog";
 import TimerModal from "../../components/TimerModal";
 const FinanceOperations = () => {
+
+
+
+  const [showItems, setShowItems] = useState(5);
+  const data = [
+    {
+      id: "faq1",
+      title: "1. What is included in Dynamics 365 Finance and Operations?",
+      content:
+        "<p>Dynamics 365 Finance and Operations is an ideal application for growing businesses to manage all their financing needs. It has various features, including accounting, finance, budgeting, project management, and more.</p>",
+    },
+    {
+      id: "faq2",
+      title:
+        "2. Do we need a minimum number of Dynamics 365 Finance & Operations licenses?",
+      content:
+        '<p>There is a minimum of 20 full users for the Finance and Operations apps. Keep in mind that it must be 20 licenses for one app, and not 10 of each or any other combination.</p>'
+    },
+    {
+      id: "faq3",
+      title:
+        "3. What kind of system software is Dynamics 365 for F and O?",
+      content: "<p>Microsoft Dynamics 365 for Finance and Operations is one of the most powerful applications to streamline all financial and operational processes. With cloud computing, it becomes more powerful, letting users connect with the operations from anywhere and anytime to gather actionable insights.</p>"
+    },
+    {
+      id: "faq4",
+      title: "4. How much is Dynamics 365 F&O?",
+      content: "<p>Generally, the full user license cost is £180/user/month. This isn't fixed for all users. The accurate cost depends on a few factors, like license type, number of users, app type, team member license, and more. However, the team at Dynamics Square can help you get an accurate amount of cost.</p>"
+    },
+    {
+      id: "faq5",
+      title: "5. Which are the major functions managed by Dynamics 365 Finance and Operations?" ,
+      content: "<p>There are various functions that aid the financing and operations of an organisation. Some functions are inventory management, cost management, product information management, transportation management, and more.</p>"
+    },
+    {
+      id: "faq6",
+      title: "6. Is there any training available by Microsoft to learn D365 Finance and Operation?",
+      content: "<p>Microsoft doesn't provide one-to-one training to its end users. However, there is lots of documentation and learning content available on Microsoft's official website. Also, the implementation partner or Dynamics 365 Finance and Operations support partner can help you understand this application.</p>"
+    },
+    {
+      id: "faq7",
+      title: "7. What are the key features of D365 for Finance and Operation?",
+      content: "<p>When it comes to features, it is far superior to its competitors. Some of the major key features are accounts receivable, fixed assets, general ledger functionality, tax, invoices, billing, and so on.</p>"
+    },
+    {
+      id: "faq8",
+      title:
+        "8. Can I upgrade my Dynamics AX to D365 F&O?",
+      content: "<p>Yes, you can easily upgrade to D365 F&amp;O. The Dynamics 365 Finance and Operations is evolved from <a target='_blank' href='/products/microsoft-dynamics-ax/'>Dynamics AX</a> (the old version) with added functionalities to cope with the ways of modern businesses. You can contact any <a target='_blank' href='/'>Microsoft-certified gold partner</a>, like Dynamics Square, to upgrade your Dynamics AX.</p>"
+    },
+    {
+      id: "faq9",
+      title:
+        "9. How do I get the trial version of D365 for F&O?",
+      content: "<p>If you are new to D365 for F&O, you can easily sign-up for a 30-day trial. However, access will be restricted as compared to a full-user license. If you are an existing customer, you can sign-up for a customer trial and create LCS (Lifecycle Service Projects) to create a development environment within it.</p>"
+    },
+    {
+      id: "faq10",
+      title: "10. Does D365 for F&O support integrations? ",
+      content: "<p>Yes, D365 for Finance and Operations allows native as well as third-party integration using APIs. Depending on the type of integration, they can help businesses with error handling, creating and updating product information, recommending a suitable solution, checking the status of customer orders, and more.</p>"
+    },
+    {
+      id: "faq11",
+      title: "11. What is D365 Finance and Operations used for? ",
+      content: "<p>D365 Finance and Operations is a cloud-based ERP solution built for medium and large-sized organisations to track all processes of inventory and finance. Currently, the license is available with two different apps, which are <a href='/products/microsoft-dynamics-365-finance/'>Dynamics 365 Finance</a> and <a target='_blank' href='/products/dynamics-365-supply-chain-management/'>Dynamics 365 Supply Chain Management</a>.</p>"
+    },
+    {
+      id: "faq12",
+      title: "12. Does D365 for Finance and Operations fit for my organization? ",
+      content: "<p>This is the best application available to streamline all financial processes if you belong to a small or medium-sized organization. Also, it helps the users monitor day-to-day operations tasks to make informed decisions whenever required.</p>"
+    },
+    {
+      id: "faq13",
+      title: "13. Does D365 for F&O enable businesses to adapt to modern compliance?",
+      content: "<p>To cope with the ways of modern business, Microsoft regularly updates the features and functionality of D365 for F&O. Generally, Microsoft releases updates two times a year and updates the cloud servers automatically without any obstruction in the current process.</p>"
+    },
+    {
+      id: "faq14",
+      title: "14. Can I implement D365 F&O?",
+      content: "<p>It would not be possible unless you had intense knowledge of coding and implementing the technologies on cloud servers. However, a Microsoft support partner like Dynamics Square can easily help you implement this application. To find out more, you can <a target='_blank' href='/contact-us/'>book a demo</a> with us.</p>"
+    },
+    {
+      id: "faq15",
+      title: "15. How long does it take to implement Dynamics 365 Finance & Operations?",
+      content: "<p>When it comes to implementing Microsoft Dynamics 365 Finance and Operations, the average time it takes is between 6-8 months (It can go up to 1 year depending upon the complexity of the business.). This can be broken down into three phases:</p><p><b>Planning and Design:</b> During the Planning and Design phase, you'll need to gather your requirements and create a project plan. This is where you'll also decide on your implementation methodology.</p><p><b>Implementation and Training:</b> During the Implementation and Training phase, you'll configure the system and train your users.</p><p><b>Go Live and Support:</b> Finally, during the GO Live and Support phase, you'll go live with the system and provide ongoing support.</p>"
+    },
+    {
+      id: "faq16",
+      title:
+        "16. Which should I implement, Microsoft Dynamics 365 F and O or Dynamics Business Central?",
+      content: "<p>There isn't any determined answer to this, and it depends on the business requirement. If you need a fully-fledged <a target='_blank' href='/products/microsoft-dynamics-erp/'>ERP solution</a> with fewer features like CRM, go for <a target='_blank' href='/products/dynamics-365-business-central/'>Dynamics BC</a>, and if you need an application to manage finance and accounting only and already have another ERP, go for Dynamics 365 F &amp; O.</p>"
+    },
+    {
+      id: "faq17",
+      title: "17. How do I get post-installation or implementation support?",
+      content: "<p>With growing businesses, errors come, whether simple or complex. But, whatever the situation, your installation partner or implementation partner will provide the needed support to get your business back on track.</p>"
+    },
+    {
+      id: "faq18",
+      title:
+        "18. How do I find the right Dynamics Finance and Operations consultant?",
+      content: "<p>This process requires much research because the partner will help you from implementation to providing timely support. Before choosing the right consultant, you must match different parameters, like experience, availability of support, cost, service charges, previous work, industry type, and more.</p>"
+    },
+    {
+      id: "faq19",
+      title: "19. What type of deployment does MS Dynamics Finance and Operations provide?",
+      content: "<p>Three deployment options are available: <a target='_blank' href='/microsoft-dynamics-365-cloud-vs-on-premise/'>on-premises</a>, cloud, and hybrid. The installation and service cost of on-premises deployment is much higher compared to cloud and hybrid deployment, and cloud deployment is the cheapest option.</p>"
+    },
+    {
+      id: "faq20",
+      title: "20. Do I need to pay extra besides the subscription fee for the cloud deployment option?",
+      content: "<p>Firstly, you must pay the application implementation cost, which totally depends on the business requirements. Besides this, you have to pay only a monthly subscription fee already decided by Microsoft and available on the website too.</p>"
+    },
+    {
+      id: "faq21",
+      title: "21. Can Dynamics 365 Finance and Operations handle multi-currencies?",
+      content: "<p>This application can manage multi-currency using Electronic Messaging and Electronic reporting features. Also, it enables the business to share the data with regulatory authorities as required by the government in some countries. </p>"
+    }
+  ];
+
+  const handleLoadMore = () => {
+    setShowItems(showItems + 4);
+  };
+
+
   return (
     <>
       <Head>
@@ -813,7 +939,67 @@ const FinanceOperations = () => {
         </div>
       </section>
     
+
       <section className="solution-faq faq">
+        <div className="container">
+          <div className="row justify-content-center">
+            <div className="col-md-8">
+            <header className="section-header">
+            <h2>Have you got questions about Dynamics 365 Finance & Operations?</h2>
+            <p>Click through to our FAQ for the best answers!</p>
+          </header>
+            </div>
+          </div>
+          <div className="row justify-content-center mar-top-7">
+            <div className="col-lg-8">
+            <div className="accordion accordion-flush" id="faqs">
+                {data.slice(0, showItems).map((item, index) => (
+                  <div key={index} className="accordion-item">
+                    <h3 className="accordion-header">
+                      <button
+                        className="accordion-button collapsed"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target={`#${item.id}`}
+                      >
+                        {item.title}
+                      </button>
+                    </h3>
+                    <div className="accordion-content"></div>
+                    <div
+                      id={item.id}
+                      className="accordion-collapse collapse"
+                      data-bs-parent="#faqs"
+                    >
+                      <div className="accordion-body">
+                        <div
+                          dangerouslySetInnerHTML={{ __html: item.content }}
+                        ></div>
+                      </div>
+                    </div>
+                  </div>
+                ))}
+
+                {showItems < data.length && (
+                  <div className="text-center">
+                    <a style={{cursor:'pointer'}}
+                      className={`load-more-btn btn-get-started scrollto ${
+                        showItems >= data.length - 4 ? "hide" : ""
+                      }`}
+                      onClick={handleLoadMore}
+                    >
+                      Load More
+                    </a>
+                  </div>
+                )}
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+
+      {/* <section className="solution-faq faq">
         <div className="container">
           <div className="row justify-content-center">
             <div className="col-md-8">
@@ -1277,7 +1463,7 @@ const FinanceOperations = () => {
             </div>
           </div>
         </div>
-      </section>
+      </section> */}
       <section className="blogs-extra">
         <div className="container">
          <div className="row justify-content-center">
