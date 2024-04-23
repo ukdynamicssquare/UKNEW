4e5dac4f84bcb7fa67ac28e65c1f732024de8d38
Dynamicssquareca <dynamicssquareca@gmail.com>
2024-04-23 16:07:50 +0530

    UPDATE

--- pages/form-test.js
+++ pages/form-test.js
@@ -7,30 +7,7 @@ const form = () => {
   return (
     <>
       <Head>
-        <title>Microsoft Dynamics NAV vs Business Central (2024) | Dynamics Square UK</title>
-        <meta
-          name="description"
-          content="Dynamics NAV is an established ERP solution. Business Central: a cloud-based, modernised version with enhanced features for SMBs. Choose based on your needs."
-        />
-        <link
-          rel="canonical"
-          href="https://www.dynamicssquare.co.uk/dynamics-nav-vs-business-central/"
-        />
-       <meta property="og:locale" content="GB" />
-        <meta property="og:type" content="website" />
-        <meta property="og:title" content="Microsoft Dynamics NAV vs Business Central (2024) | Dynamics Square UK" />
-        <meta property="og:description" content="Dynamics NAV is an established ERP solution. Business Central: a cloud-based, modernised version with enhanced features for SMBs. Choose based on your needs." />
-        <meta property="og:url" content="https://www.dynamicssquare.co.uk/products/microsoft-dynamics-365-customer-service/case-management/" />
-        <meta property="og:site_name" content="Dynamics Square UK" />
-        <meta property="og:image" content="https://www.dynamicssquare.co.uk/img/nav-vs-business-central.png" />
-        <meta property="og:image:width" content="200" />
-        <meta property="og:image:height" content="200" />
-        <meta property="og:image:type" content="image/png" />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@dsquare_uk" />
-        <meta name="twitter:title" content="Microsoft Dynamics NAV vs Business Central (2024) | Dynamics Square UK" />
-        <meta name="twitter:description" content="Dynamics NAV is an established ERP solution. Business Central: a cloud-based, modernised version with enhanced features for SMBs. Choose based on your needs." />
-        <meta name="twitter:image" content="https://www.dynamicssquare.co.uk/img/nav-vs-business-central.png" />
+      <meta name="robots" content="noindex, nofollow" />
       </Head>
 
       
