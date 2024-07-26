f9108f8e7c3058462a59da805419a5a9bf9521cf
Dynamicssquareca <dynamicssquareca@gmail.com>
2024-07-26 15:22:40 +0530

    update gtm code

--- pages/_document.js
+++ pages/_document.js
@@ -31,6 +31,16 @@ class MyDocument extends Document {
                window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'G-G7DMKRQVGJ'); 
                 `}
           </Script>
+          {/* Google Tag Manager */}
+          <Script id="GoogleTagManager" strategy="beforeInteractive">
+            {
+              `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5G8CCP6W');`
+            }
+          </Script>
           <Script id="hotjar" strategy="beforeInteractive">
             {`
                (function (h, o, t, j, a, r) {
@@ -50,10 +60,21 @@ class MyDocument extends Document {
           </Script>
         </Head>
         <body>
+          {/* Google Tag Manager (noscript) */}
+          <noscript>
+            <iframe
+              src="https://www.googletagmanager.com/ns.html?id=GTM-5G8CCP6W"
+              height="0"
+              width="0"
+              style={{ display: 'none', visibility: 'hidden' }}
+            ></iframe>
+          </noscript>
+          {/* End Google Tag Manager (noscript) */}
           <Main />
           <NextScript />
           <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
           {/* <script src="https://code-eu1.jivosite.com/widget/0vnMaZXeaH" async></script> */}
+          
           <script
             id="zsiqchat"
             async
