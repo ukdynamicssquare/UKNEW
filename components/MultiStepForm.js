35ad769b4fef9a61f81fdad0de5e0b1d6f814bd1
Dynamicssquareca <dynamicssquareca@gmail.com>
2024-10-08 21:12:46 +0530

    updaye code

--- components/MultiStepForm.js
+++ components/MultiStepForm.js
@@ -221,7 +221,7 @@ const MultiStepForm = () => {
     console.log('Submitting Data:', structuredData);
 
     // Make the API request
-    const response = await fetch('https://your-api-endpoint.com', {
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
