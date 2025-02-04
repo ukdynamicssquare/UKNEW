8f89652a5bcb78953006ff7dca5cec4397da6cdf
ukdynamicssquare <dynamicssquareuk@gmail.com>
2025-02-04 21:08:53 +0530

    update home page

--- pages/_document.js
+++ pages/_document.js
@@ -15,8 +15,9 @@ class MyDocument extends Document {
           <link href="/favicon.png" rel="icon" />
           <link href="/favicon.png" rel="apple-touch-icon" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin></link>
           <link
-            href="https://fonts.googleapis.com/css2?family=El+Messiri:wght@400;500;600;700&family=Inter:wght@400;500;600;700;800&family=Mochiy+Pop+One&family=Titillium+Web:ital,wght@0,200;0,300;0,400;0,600;0,900;1,200;1,300;1,400;1,600;1,700&display=swap"
+            href="https://fonts.googleapis.com/css2?family=El+Messiri:wght@400;500;600;700&family=Inter:wght@400;500;600;700;800&family=Devonshire&display=swap&family=Mochiy+Pop+One&family=Titillium+Web:ital,wght@0,200;0,300;0,400;0,600;0,900;1,200;1,300;1,400;1,600;1,700&display=swap"
             rel="stylesheet"
           />
           <link href="https://fonts.googleapis.com/css2?family=Anton&display=swap" rel="stylesheet"></link>
--- pages/home-one.js
+++ pages/home-one.js
@@ -1,5 +1,13 @@
-import React from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import Head from "next/head";
+import { Swiper, SwiperSlide } from 'swiper/react';
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/free-mode';
+import 'swiper/css/navigation';
+import 'swiper/css/thumbs';
+import { FreeMode, Navigation, Thumbs, Mousewheel,Autoplay } from 'swiper';
+
 
 const HomeOne = () => {
     return (
@@ -25,14 +33,82 @@ const HomeOne = () => {
                                     </div>
                                 </div>
                                 <div className='home-baner-head'>
-                                    <div className='orbit-container'></div>
-
                                     <h1>UK's Trusted Microsoft Solutions Partner</h1>
                                     <h2>We future-proof your business
                                         with scalable Microsoft solutions</h2>
                                     <p>Enabling smooth growth and transformation as your business evolves</p>
-                                    <a href="" className='btn btn-home'><img src='/img/buttion-ic-1.png' alt='icons' /> Get started <span><i className="bi bi-send"></i></span></a>
+                                    <a href="#exampleModal" data-bs-toggle="modal" className='btn btn-home'><img src='/img/buttion-ic-1.png' alt='icons' /> Get started <span><i className="bi bi-arrow-right-circle"></i></span></a>
+
                                 </div>
+                                <div className='specialtitle'>
+                                    <p>and see the magic <img src="/img/mask-group-aa.png" alt="mask-group-aa" /></p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='container-fluid'>
+                        <div className='row'>
+                            <div className='col-lg-12 p-0'>
+                                <Swiper
+                                 autoplay={{
+                                    delay: 0,
+                                    disableOnInteraction: false,
+                                    pauseOnMouseEnter: true,
+                                  }}
+                                    spaceBetween={10}
+                                    grabCursor={true}
+                                    modules={[Navigation, Mousewheel,Autoplay]}
+                                    className="mySwip"
+                                    loop={true}
+                                    speed={30000}
+                                    centeredSlides={false}
+                                    breakpoints={{
+                                        640: {
+                                            slidesPerView: 2
+                                        },
+                                        768: {
+                                            slidesPerView: 3
+                                        },
+                                        1024: {
+                                            slidesPerView: 3
+                                        },
+                                        1200: {
+                                            slidesPerView: 4
+                                        },
+                                        1400: {
+                                            slidesPerView: 5
+                                        },
+                                    }}
+                                >
+
+
+                                    <SwiperSlide>
+                                        <div className='slider0-img'>
+                                            <img src="/img/cad-i-1.png" alt="cad-i-1.png" />
+                                        </div>
+                                    </SwiperSlide>
+                                    <SwiperSlide>
+                                        <div className='slider0-img'>
+                                            <img src="/img/cad-i-2.png" alt="cad-i-1.png" />
+                                        </div>
+                                    </SwiperSlide>
+                                    <SwiperSlide>
+                                        <div className='slider0-img'>
+                                            <img src="/img/cad-i-3.png" alt="cad-i-1.png" />
+                                        </div>
+                                    </SwiperSlide>
+                                    <SwiperSlide>
+                                        <div className='slider0-img'>
+                                            <img src="/img/cad-i-4.png" alt="cad-i-1.png" />
+                                        </div>
+                                    </SwiperSlide>
+                                    <SwiperSlide>
+                                        <div className='slider0-img'>
+                                            <img src="/img/cad-i-5.png" alt="cad-i-1.png" />
+                                        </div>
+                                    </SwiperSlide>
+
+                                </Swiper>
                             </div>
                         </div>
                     </div>
