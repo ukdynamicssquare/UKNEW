95230b4e08802f4edf77dfa380f735a8a0f248f2
ukdynamicssquare <dynamicssquareuk@gmail.com>
2025-04-01 15:23:46 +0530

    update new case page

---
+++ pages/case-studies/case-new.js
@@ -0,0 +1,86 @@
+
+import Head from 'next/head';
+import React from 'react';
+
+const CaseNew = () => {
+    return (
+        <>
+            <Head>
+                <title>Case New</title>
+                <meta name="description" content="Case New Page" />
+                <link rel="canonical" href="https://www.dynamicssquare.co.uk/case-new/" />
+                <meta name="robots" content="noindex, nofollow" />
+            </Head>
+
+
+            <main>
+                <div className='case-new-banner' style={{ backgroundImage: 'url("https://www.dynamicssquare.ca/img/case-study-banner.jpg")' }}>
+                    <div className='container'>
+                        <div className='row'>
+                            <div className='col-md-5'>
+                                <div className='case-new-banner-text'>
+                                    <span>CASE STORY</span>
+                                    <h1>Transforming Business Operations
+                                        with Dynamics 365</h1>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className='case-new-content'>
+                    <div className='container'>
+                        <div className='row'>
+                            <div className='col-md-12'>
+                                <div className='case-new-content-text'>
+                                    <h2>Client Overview</h2>
+                                    <p>A leading UK-based orthopedic solutions provider sought to modernize its operations by implementing a robust ERP system. Facing challenges with disconnected processes, inefficient inventory management, and a lack of real-time financial insights, they required a solution to streamline workflows and enhance decision-making.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <section className='case-challenge'>
+                    <div className='container'>
+                        <div className='row'>
+                            <div className='col-md-12'>
+                                <div className='case-challenge-header'>
+                                    <h2>Challenges</h2>
+                                    <p>The client struggled with manual processes, leading to data inaccuracies and delays. Their existing systems lacked integration, resulting in fragmented operations and limited visibility into inventory levels and financial performance.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className='row'>
+                            <div className='col-lg-3 d-flex'>
+                                <div className='case-challenge-card gg-gray'>
+                                    <p><b>Limited Scalability:</b> The existing system struggled to handle increased business volume and complexity.</p>
+                                </div>
+                            </div>
+                            <div className='col-lg-3 d-flex'>
+                                <div className='case-challenge-card gg-gray'>
+                                    <p><b>Inefficient Processes:</b> Manual processes and data entry errors hindered productivity.</p>
+                                </div>
+                            </div>
+                            <div className='col-lg-3 d-flex'>
+                                <div className='case-challenge-card gg-gray'>
+                                    <p><b>Lack of Real-Time Insights:</b> Limited visibility into inventory levels, sales performance, and financial data.</p>
+                                </div>
+                            </div>
+                            <div className='col-lg-3 d-flex'>
+                                <div className='case-challenge-card gg-gray'>
+                                    <p><b>Integration Challenges:</b> Difficulty in integrating with other systems, such as the warehouse management system and mobile applications.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+
+
+            </main>
+
+        </>
+    );
+}
+
+export default CaseNew;
