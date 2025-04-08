7311ae60044895c3df93b2430381e2fe126de1c7
ukdynamicssquare <dynamicssquareuk@gmail.com>
2025-04-08 20:56:12 +0530

    update code

--- pages/products/new-solution.js
+++ pages/products/new-solution.js
@@ -16,7 +16,7 @@ const NewSolution = () => {
                         <div className='col-lg-8'>
                             <div className='solution-new-banner-one-content'>
                                 <h1>Microsoft Dynamics 365 Business Central</h1>
-                                <span className='large-heading'>Level up with a solution trusted by<br/>
+                                <span className='large-heading'>Level up with a solution trusted by<br />
                                     over 45,000 SMBs across the world.</span>
                                 <p>Bid adieu to data silos, the biggest culprits behind operational inefficiencies. Adopt an ERP that comes with out-of-the-box AI capabilities, Microsoft Copilot, and has less than 6 months of payback on investment.</p>
                                 <div className="text-center">
@@ -32,20 +32,26 @@ const NewSolution = () => {
                     </div>
                 </div>
                 <div className="solution-new-banner-one-img">
-                <img src="/img/n-001.png" alt="New Solution" className=" flot-1" />
+                    <img src="/img/n-001.png" alt="New Solution" className=" flot-1" />
                     <img src="/img/n-003.png" alt="New Solution" className="flot-2" />
                     <img src="/img/n-004.png" alt="New Solution" className="flot-3" />
                     <img src="/img/n-005.png" alt="New Solution" className="flot-4" />
                     <div className='flot-5'>
                         <img src="/img/menu-icons/dynamics-365-businesscentral.svg" alt="New Solution" className="flot-img" />
                     </div>
-                    {/* <div className='border-11'> 
-                        <span></span>
-                        <span></span>
-                    </div>
-                    <div className='border-12'> 
-                        <span></span>
-                        <span></span>
+                    {/* <div className='flot-121'>
+                        <div className='border-11'>
+                            <span></span>
+                            <span></span>
+                        </div>
+                        <div className='border-12'>
+                            <span></span>
+                            <span></span>
+                        </div>
+                        <div className='border-13'>
+                            <span></span>
+                          
+                        </div>
                     </div> */}
                     <div className='container'>
                         <div className='row'>
