67deb8f0a4ec894e8de8d14a1e27e410f667c6aa
ukdynamicssquare <dynamicssquareuk@gmail.com>
2025-05-16 17:16:05 +0530

    update contact code

--- pages/contact-us.js
+++ pages/contact-us.js
@@ -96,7 +96,7 @@ const ContactUs = () => {
                         <br />
                         London, EC1V 2NX, UK
                       </p>
-                      <p>+44 207 193 2502<span style={{display:'block'}}></span>+44 203 514 1057</p>
+                      <p>+44 207 193 2502<span style={{display:'block'}}></span></p>
                       <a
                         href="https://g.page/dynamics-square-london?share"
                         target="_blank"
