4ace883b4c862c7fe0cd8db542de9ffb7d72e811
ukdynamicssquare <dynamicssquareuk@gmail.com>
2025-06-02 12:57:16 +0530

    update code

--- pages/products/dynamics-365-business-central/index.js
+++ pages/products/dynamics-365-business-central/index.js
@@ -30,12 +30,10 @@ const BusinessCentral = () => {
   return (
     <>
       <Head>
-        <title>
-          Microsoft Dynamics 365 Business Central Partner - Dynamics Square UK
-        </title>
+        <title>Microsoft Dynamics 365 Business Central Partner UK</title>
         <meta
           name="description"
-          content="Simplify operations with Microsoft Dynamics 365 Business Central. Scalable cloud ERP for SMEs by Dynamics Square UK, your trusted ERP partner."
+          content="We are the UK's leading Microsoft Dynamics 365 Busienss Central partner offering our cost-effective and reliable services to SMBs. Schedule a free demo today!"
         />
         <meta name="keywords" content="Microsoft Dynamics 365 Business Central, Business Central UK, Microsoft Dynamics 365 Pricing, Business Central Team Member License, Business Dynamics 365, Dynamics 365 Business Central Essentials, ERP Microsoft Dynamics 365,Microsfot 365 Business, Microsoft Bc, Microsoft Business Central Pricing UK, Microsoft Business Central Partner, Business Central Integration, MS Dynamics Business Central, Business Central Support"></meta>
         <link
@@ -46,14 +44,14 @@ const BusinessCentral = () => {
 
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://www.dynamicssquare.co.uk/products/dynamics-365-business-central/" />
-        <meta property="og:title" content="Microsoft Dynamics 365 Business Central Partner - Dynamics Square UK" />
-        <meta property="og:description" content="Simplify operations with Microsoft Dynamics 365 Business Central. Scalable cloud ERP for SMEs by Dynamics Square UK, your trusted ERP partner." />
+        <meta property="og:title" content="Microsoft Dynamics 365 Business Central Partner UK" />
+        <meta property="og:description" content="We are the UK's leading Microsoft Dynamics 365 Busienss Central partner offering our cost-effective and reliable services to SMBs. Schedule a free demo today!" />
         <meta property="og:image" content="https://www.dynamicssquare.co.uk/img/business-center-new-banner.png" />
 
         <meta property="twitter:card" content="summary_large_image" />
         <meta property="twitter:url" content="https://www.dynamicssquare.co.uk/products/dynamics-365-business-central/" />
-        <meta property="twitter:title" content="Microsoft Dynamics 365 Business Central Partner - Dynamics Square UK" />
-        <meta property="twitter:description" content="Simplify operations with Microsoft Dynamics 365 Business Central. Scalable cloud ERP for SMEs by Dynamics Square UK, your trusted ERP partner." />
+        <meta property="twitter:title" content="Microsoft Dynamics 365 Business Central Partner UK" />
+        <meta property="twitter:description" content="We are the UK's leading Microsoft Dynamics 365 Busienss Central partner offering our cost-effective and reliable services to SMBs. Schedule a free demo today!" />
         <meta property="twitter:image" content="https://www.dynamicssquare.co.uk/img/business-center-new-banner.png" />
 
       </Head>
