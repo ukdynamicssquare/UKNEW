244a649dd90b420e0bcadb2a221cd785326c5ede
ukdynamicssquare <dynamicssquareuk@gmail.com>
2025-06-05 15:55:34 +0530

    update code

--- pages/products/dynamics-365-business-central/index.js
+++ pages/products/dynamics-365-business-central/index.js
@@ -109,7 +109,7 @@ const BusinessCentral = () => {
             <div className='col-lg-6'>
               <div className='solution-new-banner-sub-head r-top-60'>
                 <h2>What is Dynamics 365 Business Central?</h2>
-                <p>Dynamics 365 Business Central is what you need to outperform your competition and make your business thrive. It is an integrated, AI-powered, cloud-based ERP that has got you covered on all fronts: efficiency, scalability, and profitability.</p>
+                <p>Dynamics 365 Business Central is an integrated, AI-powered, cloud-based ERP that has got you covered on all fronts: efficiency, scalability, and profitability. This Microsoft ERP is what you need to outperform your competition and make your business thrive.</p>
 
                 <p>Deploy this Microsoft ERP system to give a complete makeover to your operations. Transform your business from reactive to proactive by making it agile, intelligent, and up-to-date.</p>
                 <div className="text-lg-start">
