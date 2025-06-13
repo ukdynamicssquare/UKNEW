50cadbbd12602954cfe0d392989b1c7cbd97f204
ukdynamicssquare <dynamicssquareuk@gmail.com>
2025-06-13 16:58:28 +0530

    update code

--- components/HeaderWork.js
+++ components/HeaderWork.js
@@ -21,6 +21,7 @@ const HeaderWork = () => {
     "/products/dynamics-365-business-central/warehouse-management": `${gname}`,
     "/products/dynamics-365-business-central/project-management": `${gname}`,
     "/products/dynamics-365-business-central/service-management": `${gname}`,
+       "/book-a-demo": `${gname}`,
   };
 
   const currentHeaderClass = headerClassMap[router.pathname] || "";
---
+++ pages/book-a-demo.js
@@ -0,0 +1,44 @@
+
+import Head from "next/head";
+
+const BookADemo = () => {
+    return (
+        <>
+         <Head>
+        <title>Book a demo - Dynamics Square</title>
+        <meta name="robots" content="noidex,nofollow" />
+        <meta
+          name="description"
+          content="Embark on a journey of business excellence – schedule a free demo with Dynamics Square now. Preview the transformative power of Microsoft Business Applications."
+        />
+        <link
+          rel="canonical"
+          href="https://www.dynamicssquare.co.uk/book-a-demo/"
+        />
+      </Head>
+
+      <section className="bbobg-col">
+        <div className="container">
+          <div className="row justify-content-center">
+            <div className="col-lg-9">
+              <div className="te-heading">
+                <h1>Request a Personalised Demo</h1>
+                <p>Let us show you how Microsoft Dynamics 365 can help you automate processes and boost productivity throughout your digital transformation. Our team of consultants will contact you to discuss your requirements in order to better structure the demonstration.</p>
+              </div>
+            </div>
+          </div>
+            <div className="row justify-content-center">
+                <div className="col-lg-10">
+                    <div className="booking-zoho-form">
+                        <iframe className="booking-iframe" src='https://sushilbeheradynamicssquarecom.zohobookings.in/portal-embed#/279049000000050133' frameborder='0' allowfullscreen='' ></iframe>
+                    </div>
+                </div>
+            </div>
+        </div>
+      </section>
+            
+        </>
+    );
+}
+
+export default BookADemo;
