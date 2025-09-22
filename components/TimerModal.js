9f20d9ff3dfff83b1d7f08f6e015e42486c836ea
ukdynamicssquare <dynamicssquareuk@gmail.com>
2025-09-22 21:07:19 +0530

    update TimerModal to show on desktop only and adjust modal styles for better user experience

--- components/TimerModal.js
+++ components/TimerModal.js
@@ -1,22 +1,25 @@
 import React, { useState, useEffect } from 'react';
 import Cookies from 'js-cookie';
-import Formpop from './Formpop';
 
 const TimerModal = () => {
   const [showModal, setShowModal] = useState(false);
 
   useEffect(() => {
-    const cookieExists = Cookies.get('popupModalShown');
-    if (!cookieExists) {
-      const timeoutId = setTimeout(() => {
-        setShowModal(true);
-      }, 20000); // Delay of 30 seconds
-      
-      return () => {
-        clearTimeout(timeoutId);
-      };
+    // Run only on desktop (>= 768px)
+    if (window.innerWidth >= 768) {
+      const cookieExists = Cookies.get('popupModalShown');
+      if (!cookieExists) {
+        const timeoutId = setTimeout(() => {
+          setShowModal(true);
+        }, 15000); // Delay of 15 seconds
+
+        return () => {
+          clearTimeout(timeoutId);
+        };
+      }
     }
   }, []);
+
 
   const handleCloseModal = () => {
     Cookies.set('popupModalShown', true, { expires: 1 });
@@ -26,14 +29,42 @@ const TimerModal = () => {
   return (
     <div>
       {showModal && (
-        <div className='backdrop-mod' id='popuphide'>
+        <div className='backdrop-mod backdrop-mod-d' id='popuphide'>
           <div className="popup-cust">
-          <div className="modal-content">
-            <h3>Take the First Step: Request Your Free Consultation Today!</h3>
-            <button className='bbt' onClick={handleCloseModal}><span aria-hidden="true">×</span></button>
-            <Formpop />
+            <div className="modal-content">
+              <button className='bbt' onClick={handleCloseModal}>
+                <span aria-hidden="true">×</span>
+              </button>
+
+              <a 
+                href="https://www.forbes.com/councils/forbestechcouncil/2025/09/19/how-to-integrate-generative-ai-as-a-strategic-partner/" 
+                target="_blank" 
+                rel="nofollow"
+                onClick={handleCloseModal}
+              >
+                <img src="/img/forber-03.png" alt="" />
+              </a>
+
+              <div className="buttonn-clo">
+               
+                {/* Redirect Button (target="_blank") */}
+                <a
+                  href="https://www.forbes.com/councils/forbestechcouncil/2025/09/19/how-to-integrate-generative-ai-as-a-strategic-partner/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  onClick={handleCloseModal}
+                  className="btn btn-redirect"
+                >
+                  Get Insights
+                </a>
+                 {/* Close Button */}
+                <button onClick={handleCloseModal} className="btn btn-ttt">
+                  Stay on site
+                </button>
+
+              </div>
+            </div>
           </div>
-        </div>
         </div>
       )}
     </div>
