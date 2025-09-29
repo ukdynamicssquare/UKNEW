25f4eee4fb8ee916a69af38bc1b9b4d8d67206bd
ukdynamicssquare <dynamicssquareuk@gmail.com>
2025-09-29 18:55:56 +0530

    update code

--- pages/_document.js
+++ pages/_document.js
@@ -43,16 +43,16 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             }
           </Script>
           <script
-          dangerouslySetInnerHTML={{
-            __html: `
+            dangerouslySetInnerHTML={{
+              __html: `
               (function(c,l,a,r,i,t,y){
                   c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
                   t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
                   y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
               })(window, document, "clarity", "script", "tib4o2otj4");
             `,
-          }}
-        />
+            }}
+          />
           {/* <Script id="hotjar" strategy="beforeInteractive">
             {`
                (function (h, o, t, j, a, r) {
